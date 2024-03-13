f4228d98236510a3c755c75d401244fdc6df17da
ymst <ymst@ShoheinoMacBook-Pro.local>
2024-03-14 00:23:51 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-export default nextConfig;
+import removeImports from "next-remove-imports";
+
+export default removeImports({
+  ...nextConfig,
+});
